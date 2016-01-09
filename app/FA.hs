092f22a9857898f5689650d9c918b583427435ac
max <bmx007@gmail.com>
2016-01-09 15:08:24 +0000

    Add Options

--- app/FA.hs
+++ app/FA.hs
@@ -1,3 +1,5 @@
+
+
 {-# LANGUAGE OverloadedStrings #-}
 import BasePrelude hiding((&))
 import Control.Lens
@@ -16,13 +18,77 @@ import qualified Data.Colour.Palette.BrewerSet as K
 import Data.Time
 import Data.Time.Format
 
+import qualified Options.Applicative as OP
+import Options.Applicative (Parser, strOption, long, short, metavar
+                           , help, switch, value, flag
+                           , option, auto)
+
 data ScaleMode = Log | Linear deriving (Read, Show )
 data Options = Options
   { faCredential :: !(String) -- ^ database credentials. Read format
   , scaleMode :: ScaleMode
+  , sep :: Double
+  , start :: Maybe Day
+  , end :: Maybe Day
+  , groupBy :: Maybe Period
+  , entity :: Int
+  , args :: [String]
   }
+  
+data Period = Week | Month Int | Quarter Int Int | Year Int Int
+     deriving (Show, Read) 
 
 
+optionParser :: Parser Options
+optionParser = pure Options
+  <*> strOption ( long "credential"
+                 <> short 'f'
+                 <> metavar "FILE"
+                 <> help "FrontAccounting DB credentials"
+                 <> value "credentials.cfg"
+                 )
+  <*> flag Log Linear ( long "scale-mode"
+                   <> short 'l'
+             )
+  <*>  option auto ( long "separator_width"
+                    <> metavar "SEP WIDTH"
+                    <> help "Separator witdth"
+                    <> value 0
+                    )
+  <*>  optional (fmap readDate $ strOption (long "start_date"
+                             <> short 's'
+                             <> metavar "DATE"
+                             <> help "Start date"
+       ))
+  <*>  optional (fmap readDate $ strOption (long "end_date"
+                             <> short 's'
+                             <> metavar "DATE"
+                             <> help "End date"
+       ))
+  <*> optional (fmap readPeriod $ strOption (long "group_period"
+                                    <> short 'p'
+                                    <> metavar "PERIOD .."
+                                    <> help "Grouping period"
+                                    ))
+  <*>  option auto ( long "entity_id"
+                   <> short 'e'
+                   <> metavar "ENTITY ID"
+                   <> help "entity id"
+                   )
+  <*> many (OP.argument OP.str (metavar "DIAGRAMS OPTIONS ..."))
+
+readDate :: String -> Day
+readDate str = let
+  formats = ["%D", "%F"]
+  attempts = map (parseTimeM True defaultTimeLocale) formats <*> [str]
+  in head $ catMaybes (attempts
+               ++ [error $ "Can't parse date : ["
+                         ++ str ++ "]"
+                  ]
+               )
+
+readPeriod :: String -> Period
+readPeriod s = read s 
 getDate resource = let
   n = _resName resource
   in drop (length n - 10) n
@@ -30,19 +96,20 @@ groupByYear res = let d = take 8 (getDate res)
                   in  Right $ res & resKey .~ d
                                   & resName .~ d
 
+getOptions :: IO Options
+getOptions = OP.execParser opts where
+  opts = OP.info (OP.helper <*> optionParser) OP.fullDesc
+
 main :: IO()
 main = do 
-  args <- getArgs
-  let (scaleMode, supplier_id, args') = case args of
-        ("-l":supp:args) -> (Linear, read supp, args)
-        (supp:args) -> (Log, read supp, args)
-  credentials <- read <$> readFile "credentials.cfg"
+  options <- getOptions
+  credentials <- read <$> readFile (faCredential options)
   conn <- SQL.connect credentials
-  (resources, allocations) <- loadAllocations conn supplier_id
-  let param = RenderParameter width (height scaleMode) width allocColour
-      diag = renderAllocation param _resType (orderTargets graph)
+  (resources, allocations) <- loadAllocations conn (entity options)
+  let diag = renderAllocation param _resType (orderTargets graph)
       Right graph' = buildGraph resources allocations
       graph = groupResources graph' groupByYear 
+      param = RenderParameter width (height (scaleMode options)) width allocColour
       width = 10
       height Log res box = width * log' where
              log' = max 1 (logBase 5 (abs box + 1))
@@ -54,8 +121,7 @@ main = do
       colours = cycle $ K.brewerSet K.BrBG 11 --  K.Set2 8
       resourceToColour = Map.fromList $ zip targets colours
       allocColour alloc = fromJust $ Map.lookup (_allocTarget alloc) resourceToColour
-  print graph
-  withArgs args' $ mainWith diag
+  withArgs (args options) $ mainWith diag
 
 
 supplierInvoice = 20
